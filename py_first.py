904ceb0748edb063a926c48766727ce8b6b7dd78
1454111967@qq.com <jiayou5nx>
2021-03-15 17:04:44 +0800

    第一个面向对象程序

--- py_first.py
+++ py_first.py
@@ -1,1 +1,23 @@
-print("hello")
+class Cat:
+    '''这是一个猫类'''
+
+    def eat(self):
+        print("小猫爱吃鱼")
+
+    def drink(self):
+        print("小猫在喝水")
+#创建一个猫类
+#等号右边创建对象，等号左边对新创建的对象进行引用
+tom=Cat()
+tom.drink()
+tom.eat()
+#输出tom变量的内存地址
+print(tom)
+
+#再创建一个猫类
+lazy_cat=Cat()
+lazy_cat.drink()
+lazy_cat.eat()
+print(lazy_cat)
+
+#两个猫类  tom 和 lazy_cat 是两个不同的对象  即可以有一个类，但是可有创建多个对象
